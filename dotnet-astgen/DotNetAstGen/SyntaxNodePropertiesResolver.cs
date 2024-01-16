e3d36a5429c00c86a4d8f08735f5cd975c239892
David Baker Effendi <dbe@sun.ac.za>
2024-01-16 10:46:48 +0200

    Enable Try-Catch-Finally Properties

--- dotnet-astgen/DotNetAstGen/SyntaxNodePropertiesResolver.cs
+++ dotnet-astgen/DotNetAstGen/SyntaxNodePropertiesResolver.cs
@@ -17,12 +17,13 @@ namespace DotNetAstGen
             "Alias", "NamespaceOrType", "Arguments", "Expression", "Declaration", "ElementType", "Initializer", "Else",
             "Condition", "Statement", "Statements", "Variables", "WhenNotNull", "AllowsAnyExpression", "Expressions",
             "Modifiers", "ReturnType", "IsUnboundGenericName", "Default", "IsConst", "Parameters", "Types",
-            "ExplicitInterfaceSpecifier", "MetaData", "Kind", "AstRoot", "FileName", "Code", "Operand"
+            "ExplicitInterfaceSpecifier", "MetaData", "Kind", "AstRoot", "FileName", "Code", "Operand", "Block",
+            "Catches", "Finally"
         });
 
         private readonly List<string> _regexToAllow = new(new[]
         {
-            ".*Token$", ".*Keyword$", ".*Lists?$", ".*Body$", "(Line|Column)(Start|End)", ".*Type$",
+            ".*Token$", ".*Lists?$", ".*Body$", "(Line|Column)(Start|End)", ".*Type$",
         });
 
         private readonly List<string> _regexToIgnore = new(new[]
