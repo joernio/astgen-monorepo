0a4ecf1e4f395469c351c1036b54b275aba14355
Karan Batavia <karan.batavia@privado.ai>
2024-01-19 20:14:09 +0530

    allow 'Incrementors' property

--- dotnet-astgen/DotNetAstGen/SyntaxNodePropertiesResolver.cs
+++ dotnet-astgen/DotNetAstGen/SyntaxNodePropertiesResolver.cs
@@ -18,7 +18,7 @@ namespace DotNetAstGen
             "Condition", "Statement", "Statements", "Variables", "WhenNotNull", "AllowsAnyExpression", "Expressions",
             "Modifiers", "ReturnType", "IsUnboundGenericName", "Default", "IsConst", "Parameters", "Types",
             "ExplicitInterfaceSpecifier", "MetaData", "Kind", "AstRoot", "FileName", "Code", "Operand", "Block",
-            "Catches", "Finally", "Keyword"
+            "Catches", "Finally", "Keyword", "Incrementors"
         });
 
         private readonly List<string> _regexToAllow = new(new[]
