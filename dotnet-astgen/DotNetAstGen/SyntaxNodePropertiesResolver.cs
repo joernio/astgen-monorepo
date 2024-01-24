13134090b10dfd169b2d615650e5e33748e61849
Karan Batavia <karan.batavia@privado.ai>
2024-01-24 14:53:56 +0530

    allow Labels property

--- dotnet-astgen/DotNetAstGen/SyntaxNodePropertiesResolver.cs
+++ dotnet-astgen/DotNetAstGen/SyntaxNodePropertiesResolver.cs
@@ -18,7 +18,7 @@ namespace DotNetAstGen
             "Condition", "Statement", "Statements", "Variables", "WhenNotNull", "AllowsAnyExpression", "Expressions",
             "Modifiers", "ReturnType", "IsUnboundGenericName", "Default", "IsConst", "Parameters", "Types",
             "ExplicitInterfaceSpecifier", "MetaData", "Kind", "AstRoot", "FileName", "Code", "Operand", "Block",
-            "Catches", "Finally", "Keyword", "Incrementors", "Sections", "Pattern"
+            "Catches", "Finally", "Keyword", "Incrementors", "Sections", "Pattern", "Labels"
         });
 
         private readonly List<string> _regexToAllow = new(new[]
