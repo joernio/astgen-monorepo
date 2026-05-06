b7019839c58c35db8f1b2cc5c12b2bc6adceb226
Xavier Pinho <mail@xavierp.org>
2026-05-06 02:31:47 +0100

    experiment with flaky assertion

--- rust-astgen/tests/names.rs
+++ rust-astgen/tests/names.rs
@@ -320,9 +320,9 @@ fn main() {
     );
 
     // TODO: this is being flaky in CI... don't understand why.
-    //let dyn_sink_call = method_call_expr(&json, "s.sink()").on_line("let dyn_value = s.sink();");
-    //assert_eq!(dyn_sink_call.type_full_name(), "u32");
-    //assert_eq!(dyn_sink_call.method_full_name(), "rust2cpg::Sink<T>::sink");
+    let dyn_sink_call = method_call_expr(&json, "s.sink()").on_line("let dyn_value = s.sink();");
+    assert_eq!(dyn_sink_call.type_full_name(), "u32");
+    assert_eq!(dyn_sink_call.method_full_name(), "rust2cpg::Sink<T>::sink");
 
     assert_eq!(
         ident_pat(&json, "value")
