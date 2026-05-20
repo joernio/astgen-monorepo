876341f3c848a5bbae65dfe67a0afd2c3001ee2c
Xavier Pinho <mail@xavierp.org>
2026-05-20 11:45:12 +0100

    [rust_ast_gen] emit scala `methodFullName` and `typeFullName` accessors

---
+++ rust-astgen/scala_tests/src/test/scala/io/joern/rust2cpg/parser/RustAstGenFullNameTests.scala
@@ -0,0 +1,46 @@
+package io.joern.rust2cpg.parser
+
+import io.joern.rust2cpg.parser.RustNodeSyntax.{
+  CallExpr,
+  Fn,
+  IdentPat,
+  LetStmt
+}
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers.*
+
+class RustAstGenFullNameTests extends AnyFunSuite with RustAstGenTestFixture {
+
+  test("methodFullName/typeFullName from JSON") {
+    val srcFile = code(
+      """
+        |fn add(a: i32, b: i32) -> i32 { a + b }
+        |fn main() { let x = add(1, 2); }
+        |""".stripMargin)
+
+    val letStmt: LetStmt = srcFile.item
+      .collect { case fn: Fn => fn }
+      .flatMap(_.blockExpr)
+      .flatMap(_.stmtList.stmt)
+      .collectFirst { case ls: LetStmt => ls }
+      .getOrElse(fail("expected let statement"))
+
+    val identPat: IdentPat = letStmt.pat match {
+      case ident: IdentPat => ident
+      case other           => fail(s"expected IdentPat, got ${other.getClass.getSimpleName}")
+    }
+    identPat.typeFullName shouldBe Some("i32")
+
+    val callExpr: CallExpr = letStmt.expr match {
+      case ce: CallExpr => ce
+      case other        => fail(s"expected CallExpr, got ${other.getClass.getSimpleName}")
+    }
+    callExpr.methodFullName shouldBe Some("rust_ast_gen_scala_test::add")
+    callExpr.typeFullName shouldBe Some("i32")
+
+    val lParen = callExpr.argList.lParenToken
+    lParen.methodFullName shouldBe None
+    lParen.typeFullName shouldBe None
+  }
+
+}
