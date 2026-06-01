aca591d09ce7ab4157bf11e8a6c11a4ea7caab06
Xavier Pinho <mail@xavierp.org>
2026-06-01 08:45:33 +0100

    [rust_ast_gen] initial macro expansion support (#50)

--- rust-astgen/scala_tests/src/test/scala/io/joern/rust2cpg/parser/RustAstGenPrettyPrintTests.scala
+++ rust-astgen/scala_tests/src/test/scala/io/joern/rust2cpg/parser/RustAstGenPrettyPrintTests.scala
@@ -1,5 +1,7 @@
 package io.joern.rust2cpg.parser
 
+import io.joern.rust2cpg.parser.RustNodeSyntax.{BlockExpr, CallExpr, ExprStmt, Literal, MacroStmts}
+import org.scalatest.Inside.inside
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers.shouldBe
 
@@ -112,4 +114,148 @@ class RustAstGenPrettyPrintTests extends AnyFunSuite with RustAstGenTestFixture 
 
   }
 
+  test("stringify! expansion") {
+    val snippet =
+      """
+        |fn main() {
+        | let s = stringify!(hello world);
+        |}
+        |""".stripMargin
+
+    val srcFile = code(snippet, noSysRoot = false)
+
+    srcFile.prettyPrint shouldBe
+      """SOURCE_FILE
+        |  FN
+        |    FN_KW
+        |    NAME
+        |      IDENT
+        |    PARAM_LIST
+        |      L_PAREN
+        |      R_PAREN
+        |    BLOCK_EXPR
+        |      STMT_LIST
+        |        L_CURLY
+        |        LET_STMT
+        |          LET_KW
+        |          IDENT_PAT
+        |            NAME
+        |              IDENT
+        |          EQ
+        |          MACRO_EXPR
+        |            MACRO_CALL
+        |              PATH
+        |                PATH_SEGMENT
+        |                  NAME_REF
+        |                    IDENT
+        |              BANG
+        |              TOKEN_TREE
+        |                L_PAREN
+        |                IDENT
+        |                IDENT
+        |                R_PAREN
+        |          SEMICOLON
+        |        R_CURLY""".stripMargin
+
+    inside(macroCalls(srcFile)) {
+      case stringify :: Nil =>
+        stringify.textFrom(snippet) shouldBe "stringify!(hello world)"
+        inside(stringify.macroExpansion) {
+          case Some(lit: Literal) =>
+            lit.typeFullName shouldBe Some("&str")
+            // Not ideal, but just documenting the current status quo: we don't have a good way of getting
+            // the expanded nodes' ranges.
+            lit.textFrom(snippet) shouldBe ""
+        }
+    }
+  }
+
+  test("vec! and println! expansion") {
+    val snippet =
+      """
+        |fn main() {
+        | let v = vec![1, 2, 3];
+        | println!(v);
+        |}
+        |""".stripMargin
+    val srcFile = code(snippet, noSysRoot = false)
+
+    inside(macroCalls(srcFile)) {
+      case vec :: print :: Nil =>
+        vec.textFrom(snippet) shouldBe "vec![1, 2, 3]"
+        print.textFrom(snippet) shouldBe "println!(v)"
+
+        inside(vec.macroExpansion) {
+          case Some(call: CallExpr) =>
+            call.methodFullName shouldBe Some("alloc::boxed::box_assume_init_into_vec_unsafe<T, N>")
+            call.typeFullName shouldBe Some("alloc::vec::Vec<i32, alloc::alloc::Global>")
+        }
+
+        inside(print.macroExpansion) {
+          case Some(macroStmts: MacroStmts) =>
+            macroStmts.stmt shouldBe Nil
+            inside(macroStmts.expr) {
+              case Some(blockExpr: BlockExpr) =>
+                inside(blockExpr.stmtList.stmt) {
+                  case (exprStmt: ExprStmt) :: Nil =>
+                    inside(exprStmt.expr) {
+                      case callExpr: CallExpr =>
+                        callExpr.methodFullName shouldBe Some("std::io::stdio::_print")
+                        callExpr.typeFullName shouldBe Some("()")
+                    }
+                }
+            }
+        }
+    }
+
+    srcFile.prettyPrint shouldBe
+      """SOURCE_FILE
+        |  FN
+        |    FN_KW
+        |    NAME
+        |      IDENT
+        |    PARAM_LIST
+        |      L_PAREN
+        |      R_PAREN
+        |    BLOCK_EXPR
+        |      STMT_LIST
+        |        L_CURLY
+        |        LET_STMT
+        |          LET_KW
+        |          IDENT_PAT
+        |            NAME
+        |              IDENT
+        |          EQ
+        |          MACRO_EXPR
+        |            MACRO_CALL
+        |              PATH
+        |                PATH_SEGMENT
+        |                  NAME_REF
+        |                    IDENT
+        |              BANG
+        |              TOKEN_TREE
+        |                L_BRACK
+        |                INT_NUMBER
+        |                COMMA
+        |                INT_NUMBER
+        |                COMMA
+        |                INT_NUMBER
+        |                R_BRACK
+        |          SEMICOLON
+        |        EXPR_STMT
+        |          MACRO_EXPR
+        |            MACRO_CALL
+        |              PATH
+        |                PATH_SEGMENT
+        |                  NAME_REF
+        |                    IDENT
+        |              BANG
+        |              TOKEN_TREE
+        |                L_PAREN
+        |                IDENT
+        |                R_PAREN
+        |          SEMICOLON
+        |        R_CURLY""".stripMargin
+
+  }
 }
--- rust-astgen/src/json_ast.rs
+++ rust-astgen/src/json_ast.rs
@@ -1,9 +1,9 @@
 //! The actual JSON shape we emit per Rust source file.
 
 use crate::names::{method_full_name_for_node, type_full_name_for_node};
-use ra_ap_hir::Semantics;
-use ra_ap_ide::{LineIndex, RootDatabase};
-use ra_ap_syntax::{NodeOrToken, SyntaxNode, SyntaxToken};
+use ra_ap_hir::{HirFileId, Semantics};
+use ra_ap_ide::{LineIndex, RootDatabase, TextRange};
+use ra_ap_syntax::{AstNode, NodeOrToken, SyntaxNode, SyntaxToken, ast};
 use serde::Serialize;
 
 /// Per-file envelope wrapping the AST.
@@ -37,6 +37,9 @@ pub(crate) struct RustAstGenJsonNode {
     pub(crate) method_full_name: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) type_full_name: Option<String>,
+    // Only applicable when node_kind is MacroCall.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) macro_expansion: Option<Box<RustAstGenJsonNode>>,
     pub(crate) children: Vec<RustAstGenJsonNode>,
 }
 
@@ -51,8 +54,10 @@ pub(crate) struct RustAstGenJsonNodeRange {
 }
 
 impl RustAstGenJsonNodeRange {
-    pub(crate) fn from_node(node: &SyntaxNode, line_index: &LineIndex) -> Self {
-        let text_range = node.text_range();
+    // NB: remember that macro expansion ranges are not meaningful in the JSON AST,
+    // since they are expanded in a separate virtual file (which is not emitted.)
+    // This is just a helper to build the JsonNodeRange.
+    fn from_text_range(text_range: TextRange, line_index: &LineIndex) -> Self {
         let start = text_range.start();
         let end = text_range.end();
         let start_line_col = line_index.line_col(start);
@@ -65,62 +70,90 @@ impl RustAstGenJsonNodeRange {
         }
     }
 
-    pub(crate) fn from_token(token: &SyntaxToken, line_index: &LineIndex) -> Self {
-        let text_range = token.text_range();
-        let start = text_range.start();
-        let end = text_range.end();
-        let start_line_col = line_index.line_col(start);
-
+    fn empty() -> Self {
         Self {
-            start_offset: u32::from(start),
-            end_offset: u32::from(end),
-            start_line: start_line_col.line,
-            start_column: start_line_col.col,
+            start_offset: 0,
+            end_offset: 0,
+            start_line: 0,
+            start_column: 0,
         }
     }
 }
 
 impl RustAstGenJsonNode {
+    // NB: we need the hir_file_id to know if the current node is coming from
+    // a macro expansion. (Macros are expanded in a separate virtual file.)
     pub(crate) fn from_node(
         node: &SyntaxNode,
+        hir_file_id: HirFileId,
         line_index: &LineIndex,
         semantics: &Semantics<RootDatabase>,
     ) -> Self {
         let node_kind = format!("{:?}", node.kind());
-        let range = RustAstGenJsonNodeRange::from_node(node, line_index);
+        let range = Self::make_range(node.text_range(), hir_file_id, line_index);
         let method_full_name = method_full_name_for_node(node, semantics);
         let type_full_name = type_full_name_for_node(node, semantics);
+
+        let macro_expansion = ast::MacroCall::cast(node.clone())
+            .and_then(|macro_call| semantics.expand_macro_call(&macro_call))
+            .map(|expanded| {
+                Self::from_node(&expanded.value, expanded.file_id, line_index, semantics).into()
+            });
+
         let children = node
             .children_with_tokens()
             .filter(|child| !child.kind().is_trivia())
             .map(|node_or_token| match node_or_token {
                 NodeOrToken::Node(child_node) => {
-                    Self::from_node(&child_node, line_index, semantics)
+                    Self::from_node(&child_node, hir_file_id, line_index, semantics)
                 }
-                NodeOrToken::Token(child_token) => Self::from_token(&child_token, line_index),
+                NodeOrToken::Token(child_token) => {
+                    Self::from_token(&child_token, hir_file_id, line_index)
+                }
             })
             .collect();
 
         Self {
             node_kind,
             range,
+            macro_expansion,
             method_full_name,
             type_full_name,
             children,
         }
     }
 
-    pub(crate) fn from_token(token: &SyntaxToken, line_index: &LineIndex) -> Self {
+    pub(crate) fn from_token(
+        token: &SyntaxToken,
+        hir_file_id: HirFileId,
+        line_index: &LineIndex,
+    ) -> Self {
         let node_kind = format!("{:?}", token.kind());
-        let range = RustAstGenJsonNodeRange::from_token(token, line_index);
+        let range = Self::make_range(token.text_range(), hir_file_id, line_index);
         let children = vec![];
 
         Self {
             node_kind,
             range,
             children,
+            macro_expansion: None,
             method_full_name: None,
             type_full_name: None,
+        }
+    }
+
+    // Inside a macro expansion, ranges are relative to the macro expansion file.
+    // This gets worse as macros can expand into other macros.
+    // So, macro-expanded nodes/tokens have empty ranges.
+    fn make_range(
+        text_range: TextRange,
+        hir_file_id: HirFileId,
+        line_index: &LineIndex,
+    ) -> RustAstGenJsonNodeRange {
+        if hir_file_id.is_macro() {
+            RustAstGenJsonNodeRange::empty()
+        } else {
+            RustAstGenJsonNodeRange::from_text_range(text_range, line_index)
         }
     }
 }
