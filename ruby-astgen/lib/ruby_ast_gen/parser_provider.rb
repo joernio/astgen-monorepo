e0284c98a6c71f160a0e1f014efe8c3d642d98e6
Tebogo Selahle <tn.selahle@gmail.com>
2026-07-14 16:22:35 +0200

    Update log messages

--- ruby-astgen/lib/ruby_ast_gen/parser_provider.rb
+++ ruby-astgen/lib/ruby_ast_gen/parser_provider.rb
@@ -8,14 +8,14 @@ module RubyAstGen
         require "prism"
         return Prism::Translation::Parser.new.parse(buffer)
       rescue StandardError => e
-        RubyAstGen::Logger::warn "Prism parser failed: #{e.class} - #{e.message}, trying parser gem"
+        RubyAstGen::Logger::warn "Prism parser failed: #{e.class} - #{e.message}, trying whitequark parser gem"
       end
 
       begin
         require "parser/current"
         Parser::CurrentRuby.new.parse(buffer)
       rescue StandardError => e
-        RubyAstGen::Logger::error "Parser gem also failed: #{e.class} - #{e.message}"
+        RubyAstGen::Logger::error "Whitequark parser gem also failed: #{e.class} - #{e.message}"
         nil
       end
     end
